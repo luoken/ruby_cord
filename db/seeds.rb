922a16d04c9f1150e1f283b806dae01e3d9c7fe7
Ken <kluo.1030@gmail.com>
2025-05-20 10:43:16 -0700

    adds migration for chat and users; starts auth process

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(username: "Bob", password_digest: "password")
+User.create(username: "Anne", password_digest: "password")
+User.create(username: "Candace", password_digest: "password")
+User.create(username: "Dilbert", password_digest: "password")
+User.create(username: "Grace", password_digest: "password")
+
+Message.create(body: "Hello", user_id: 1)
+Message.create(body: "Hello", user_id: 2)
+Message.create(body: "What up", user_id: 3)
+Message.create(body: "Hey", user_id: 4)
